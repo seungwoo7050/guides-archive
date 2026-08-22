11ca24c57e44bbcdf8fc105dcc4e0bc74b32a711
seungwoo7050 <seungwoo7050@naver.com>
2026-08-23 07:20:13 +0900

    feat: local-git-lab sample repository 구성

--- exercises/local-git-lab/git-lab.sh
+++ exercises/local-git-lab/git-lab.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-# [Implementation 1] [Implementation 2] [Implementation 3] [Implementation 4]
+# [Implementation 1] [Implementation 2] [Implementation 3] [Implementation 4] [Implementation 5]
 case "${1:---help}" in -h|--help) echo "local-git-lab historical stage";; *) mkdir -p lab;; esac
