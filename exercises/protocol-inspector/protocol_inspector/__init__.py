fee21afa5c2f2e36816c78ba39745fedd55df1a2
seungwoo7050 <seungwoo7050@naver.com>
2026-08-23 07:20:10 +0900

    feat: protocol-inspector CLI와 project composition 완성

--- exercises/protocol-inspector/protocol_inspector/__init__.py
+++ exercises/protocol-inspector/protocol_inspector/__init__.py
@@ -1,10 +1,44 @@
-"""Public modules available at this historical stage."""
+"""protocol-inspector에서 제공하는 공개 API를 모읍니다."""
 
-from . import checksum
-from . import packet
-from . import errors
-from . import pcap
-from . import routing
-from . import tcp_state
+from .checksum import checksum_is_valid, internet_checksum, tcp_checksum_ipv4
+from .errors import InvalidTransition, PacketFormatError
+from .packet import (
+    DecodedPacket,
+    EthernetFrame,
+    IPv4Packet,
+    TCPSegment,
+    VlanTag,
+    decode_ethernet_ipv4_tcp,
+    parse_ethernet,
+    parse_ipv4,
+    parse_tcp,
+)
+from .pcap import Capture, CapturedPacket, parse_pcap
+from .routing import Route, RoutingTable
+from .tcp_state import EndpointRole, TCPEndpoint, TCPEvent, TCPState
 
-__all__ = ['checksum', 'packet', 'errors', 'pcap', 'routing', 'tcp_state']
+__all__ = [
+    "Capture",
+    "CapturedPacket",
+    "DecodedPacket",
+    "EndpointRole",
+    "EthernetFrame",
+    "IPv4Packet",
+    "InvalidTransition",
+    "PacketFormatError",
+    "Route",
+    "RoutingTable",
+    "TCPEndpoint",
+    "TCPEvent",
+    "TCPSegment",
+    "TCPState",
+    "VlanTag",
+    "checksum_is_valid",
+    "decode_ethernet_ipv4_tcp",
+    "internet_checksum",
+    "parse_ethernet",
+    "parse_ipv4",
+    "parse_pcap",
+    "parse_tcp",
+    "tcp_checksum_ipv4",
+]
