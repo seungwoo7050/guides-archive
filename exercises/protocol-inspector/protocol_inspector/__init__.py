c4a04197b2c58f23ccce8c3dd25dd67a52655e17
seungwoo7050 <seungwoo7050@naver.com>
2026-08-23 07:20:09 +0900

    feat: protocol-inspector longest-prefix route lookup 구현

--- exercises/protocol-inspector/protocol_inspector/__init__.py
+++ exercises/protocol-inspector/protocol_inspector/__init__.py
@@ -4,5 +4,6 @@ from . import checksum
 from . import packet
 from . import errors
 from . import pcap
+from . import routing
 
-__all__ = ['checksum', 'packet', 'errors', 'pcap']
+__all__ = ['checksum', 'packet', 'errors', 'pcap', 'routing']
